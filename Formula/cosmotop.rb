ff49e5016c083211e0321870e912d07f9dfb50f0
Brett Jia <dev.bjia56@gmail.com>
2025-05-10 10:27:08 -0400

    add formula for cosmotop

---
+++ Formula/cosmotop.rb
@@ -0,0 +1,16 @@
+class Cosmotop < Formula
+  desc "Multiplatform system monitoring tool using Cosmopolitan Libc"
+  homepage "https://github.com/bjia56/cosmotop"
+  version "0.7.1"
+  url "https://github.com/bjia56/cosmotop/releases/download/v#{version}/cosmotop.exe"
+  sha256 "1f533b1de706ff0f00edbc73787fd8a3cda8aa38ce269860e4abf146b8850c02"
+  license "Apache-2.0"
+
+  def install
+    bin.install_symlink "cosmotop.exe" => "cosmotop"
+  end
+
+  test do
+    assert_match "cosmotop", shell_output("#{bin}/cosmotop --version", 1)
+  end
+end

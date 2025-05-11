df7835023f7798f198c47ed73e818c95a47a149b
Brett Jia <dev.bjia56@gmail.com>
2025-05-11 15:34:41 -0400

    update workflow and make version v-prefixed

--- Formula/cosmotop.rb
+++ Formula/cosmotop.rb
@@ -2,7 +2,7 @@ class Cosmotop < Formula
   desc "Multiplatform system monitoring tool using Cosmopolitan Libc"
   homepage "https://github.com/bjia56/cosmotop"
   version "0.7.1"
-  url "https://github.com/bjia56/cosmotop/releases/download/v#{version}/cosmotop.exe"
+  url "https://github.com/bjia56/cosmotop/releases/download/#{version}/cosmotop.exe"
   sha256 "1f533b1de706ff0f00edbc73787fd8a3cda8aa38ce269860e4abf146b8850c02"
   license "Apache-2.0"
 

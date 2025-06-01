cfcb27394061ed5ece20137070b434bb915a1745
Brett Jia <dev.bjia56@gmail.com>
2025-06-01 09:56:03 -0400

    simplify shim scripts in preparation for 0.8.0

--- Formula/cosmotop.rb
+++ Formula/cosmotop.rb
@@ -2,18 +2,17 @@ class Cosmotop < Formula
   desc "Multiplatform system monitoring tool using Cosmopolitan Libc"
   homepage "https://github.com/bjia56/cosmotop"
   version "v0.7.1"
-  url "https://github.com/bjia56/cosmotop/releases/download/#{version}/cosmotop.exe"
+  url "https://github.com/bjia56/cosmotop/releases/download/#{version}/cosmotop"
   sha256 "1f533b1de706ff0f00edbc73787fd8a3cda8aa38ce269860e4abf146b8850c02"
   license "Apache-2.0"
 
   def install
-    libexec.install "cosmotop.exe"
+    libexec.install "cosmotop"
     launcher = <<~EOS
       #!/bin/bash
-      chmod +x "#{prefix}/libexec/cosmotop.exe" >/dev/null 2>&1
-      exec "#{prefix}/libexec/cosmotop.exe" "$@"
+      chmod +x "#{prefix}/libexec/cosmotop" >/dev/null 2>&1
+      exec sh "#{prefix}/libexec/cosmotop" "$@"
     EOS
     (bin/"cosmotop").write launcher
-    (bin/"cosmotop.exe").write launcher
   end
 end

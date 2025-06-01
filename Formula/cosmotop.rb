659b5294d7d0e1a02e3f5312967cde9d8dd935ef
Brett Jia <dev.bjia56@gmail.com>
2025-06-01 09:55:16 -0400

    Revert "Update cosmotop formula to v0.8.0 (#2)"
    This reverts commit e0ea758c4164d30ab0b38810c2c012024f050e55.

--- Formula/cosmotop.rb
+++ Formula/cosmotop.rb
@@ -1,12 +1,19 @@
 class Cosmotop < Formula
   desc "Multiplatform system monitoring tool using Cosmopolitan Libc"
   homepage "https://github.com/bjia56/cosmotop"
-  version "v0.8.0"
-  url "https://github.com/bjia56/cosmotop/releases/download/#{version}/cosmotop"
-  sha256 "9bd8d7720a0bf8b0085da65f05ea8419bb79b4987549d21779a0c65a8a568813"
+  version "v0.7.1"
+  url "https://github.com/bjia56/cosmotop/releases/download/#{version}/cosmotop.exe"
+  sha256 "1f533b1de706ff0f00edbc73787fd8a3cda8aa38ce269860e4abf146b8850c02"
   license "Apache-2.0"
 
   def install
-    libexec.install "cosmotop"
+    libexec.install "cosmotop.exe"
+    launcher = <<~EOS
+      #!/bin/bash
+      chmod +x "#{prefix}/libexec/cosmotop.exe" >/dev/null 2>&1
+      exec "#{prefix}/libexec/cosmotop.exe" "$@"
+    EOS
+    (bin/"cosmotop").write launcher
+    (bin/"cosmotop.exe").write launcher
   end
 end

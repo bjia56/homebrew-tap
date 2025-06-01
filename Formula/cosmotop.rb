203484352c2adc5cd696ea2796be5d9d7ffb4843
Brett Jia <dev.bjia56@gmail.com>
2025-06-01 09:48:26 -0400

    update install process to remove shim scripts

--- Formula/cosmotop.rb
+++ Formula/cosmotop.rb
@@ -2,18 +2,11 @@ class Cosmotop < Formula
   desc "Multiplatform system monitoring tool using Cosmopolitan Libc"
   homepage "https://github.com/bjia56/cosmotop"
   version "v0.8.0"
-  url "https://github.com/bjia56/cosmotop/releases/download/#{version}/cosmotop.exe"
+  url "https://github.com/bjia56/cosmotop/releases/download/#{version}/cosmotop"
   sha256 "9bd8d7720a0bf8b0085da65f05ea8419bb79b4987549d21779a0c65a8a568813"
   license "Apache-2.0"
 
   def install
-    libexec.install "cosmotop.exe"
-    launcher = <<~EOS
-      #!/bin/bash
-      chmod +x "#{prefix}/libexec/cosmotop.exe" >/dev/null 2>&1
-      exec "#{prefix}/libexec/cosmotop.exe" "$@"
-    EOS
-    (bin/"cosmotop").write launcher
-    (bin/"cosmotop.exe").write launcher
+    libexec.install "cosmotop"
   end
 end
